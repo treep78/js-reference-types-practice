72bb648c844ff1790117dc4ded066c7869ab10ef
Antony Donovan <antony.donovan@generalassemb.ly>
2015-10-01 16:23:02 -0400

    Clean up README.md and Subway lines.
    Testing all working.

---
+++ README.md
@@ -0,0 +1,40 @@
+#Title: MBTA Homework
+
+## To start
+
+Fork, clone, branch (practice) and npm install
+
+## Objectives:
+- Practice with functions, arrays, and properties of an object literal.
+- You may submit a pull request for this practive, but it is not required.
+
+## Activity:
+
+- Create a function to calculate the number of stops between stations on the "MBTA". Write additional functions used by this function as needed.
+- The function takes the line and stop that a rider is getting on at and the line and stop that user is getting off at and **returns the total number of stops for the trip**.
+
+There are 3 subway lines:
+
+- The Red line has the following stops: South Station, Park Street, Kendall, Central, Harvard, Porter, Davis, Alewife
+- The Green line has the following stops: Government Center, Park Street, Bolyston, Arlington, Copley, Hynes, Kenmore
+- The Orange line has the following stops:  North Station, Haymarket, Park Street, State, Downtown Crossing, Chinatown, Back Bay, Forest Hills
+- All 3 subway lines intersect at *Park Street*, but there are no other intersection points. Some of this MBTA is fictionalized.
+
+## Checking your solution
+
+Run `grunt test`.
+
+## Hints:
+
+* Assume good input.  Your function need not check the validity of the line or stop.
+* Consider diagramming the lines by sketching out the subway lines and their stops and intersection.
+* Subway lines are properties in an object literal (hashmap), while the values are an array of all the stops on each line.
+* The key to the lab is to find the __intersection__ of the lines at *Park Street*.
+* Solve an easier problem first.
+
+## Bonus
+
+Think about adding Haymarket to the Green line.  Try to find a solution for multiple intersections.  Is there a way to decide is an answer is "correct"?
+
+This bonus is completely optional and does not have tests, yet.
+
---
+++ spec/mbta.spec.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var mbta = require('../lib/mbta.js');
+
+describe('mbta', function() {
+
+  describe('Red Line', function() {
+
+    it('goes from "Alewife" to "South Station"', function() {
+      expect(mbta.stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station')).toBe(7);
+    });
+
+    it('goes from "South Station" to "Alewife"', function() {
+      expect(mbta.stopsBetweenStations('Red', 'South Station', 'Red', 'Alewife')).toBe(7);
+    });
+
+  });
+
+  describe('Green Line', function() {
+
+    it('goes from "haymarket" to "copley"', function() {
+      expect(mbta.stopsBetweenStations('Green', 'Government Center', 'Green', 'Kenmore')).toBe(6);
+    });
+
+    it('goes from "copley" to "haymarket"', function() {
+      expect(mbta.stopsBetweenStations('Green', 'Kenmore', 'Green', 'Government Center')).toBe(6);
+    });
+
+  });
+
+  describe('Orange Line', function() {
+
+    it('goes from "North Station" to "Forest Hills"', function() {
+      expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Orange', 'Forest Hills')).toBe(7);
+    });
+
+    it('goes from "Forest Hills" to "North Station"', function() {
+      expect(mbta.stopsBetweenStations('Orange', 'Forest Hills', 'Orange', 'North Station')).toBe(7);
+    });
+
+  });
+
+  describe('Red and Green Lines', function() {
+
+    it('goes from "South Station" to "Kenmore"', function() {
+      expect(mbta.stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore')).toBe(6);
+    });
+
+    it('goes from "Government Center" to "Alewife"', function() {
+      expect(mbta.stopsBetweenStations('Green', 'Government Center', 'Red', 'Alewife')).toBe(7);
+    });
+
+  });
+
+  describe('Red and Orange Lines', function() {
+
+    it('goes from "South Station" to "Forest Hills"', function() {
+      expect(mbta.stopsBetweenStations('Red', 'South Station', 'Orange', 'Forest Hills')).toBe(6);
+    });
+
+    it('goes from "North Station" to "Alewife"', function() {
+      expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Red', 'Alewife')).toBe(8);
+    });
+
+  });
+
+  describe('Green and Orange Lines', function() {
+
+    it('goes from "Government Center" to "Forest Hills"', function() {
+      expect(mbta.stopsBetweenStations('Green', 'Government Center', 'Orange', 'Forest Hills')).toBe(6);
+    });
+
+    it('goes from "North Station" to "Kenmore"', function() {
+      expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Green', 'Kenmore')).toBe(7);
+    });
+
+  });
+
+  if (mbta.stretch) {
+
+    describe('Green and Orange Lines', function() {
+
+    });
+
+  }
+
+});
